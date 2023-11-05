e440e220fd89da61dda6d55710c4419ba37194a6
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-11-05 18:57:46 +0000

    Feat: Create model for user account

---
+++ backend/src/main/java/com/example/pomodoroApp/model/userAccount.java
@@ -0,0 +1,33 @@
+package com.example.pomodoroApp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class userAccount {
+
+    @Id
+    @GeneratedValue
+    @Column(updatable = false, nullable = false)
+    Long userId;
+
+    @Column(updatable = false, nullable = false)
+    String googleUserId;
+
+    @Column(updatable = false, nullable = false)
+    String userName;
+
+    @Column(updatable = false, nullable = false)
+    String userEmail;
+
+}
