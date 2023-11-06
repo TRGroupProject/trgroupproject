3a5d04026b27f780044a345598643e11ffd89726
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-11-06 00:16:10 +0000

    Refactor: Rename files for controllers

--- backend/src/main/java/com/example/pomodoroApp/controller/PomodoroTasksApiController.java
+++ backend/src/main/java/com/example/pomodoroApp/controller/PomodoroTasksApiController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/events")
-public class PomodoroCalendarApiController {
+public class PomodoroTasksApiController {
 
     @GetMapping("/")
     public String greeting() {
