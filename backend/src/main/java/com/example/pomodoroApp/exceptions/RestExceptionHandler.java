eed3d4503baa5622cf2c16b913cc0543951adb34
Adrian Yates <118460049+adeyatz@users.noreply.github.com>
2023-11-06 19:15:20 +0000

    feat: RestExceptionHandler class to handle API Rest exceptions

---
+++ backend/src/main/java/com/example/pomodoroApp/exceptions/RestExceptionHandler.java
@@ -0,0 +1,58 @@
+package com.example.pomodoroApp.exceptions;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler  {
+    @ExceptionHandler({InvalidTaskIdException.class})
+    protected ResponseEntity<Object> handleInvalidTaskIdException (InvalidTaskIdException exception, WebRequest request) {
+        return handleExceptionInternal(exception,
+                request.getDescription(false) + " : " +  exception.getMessage(),
+                new HttpHeaders(),
+                HttpStatus.NOT_FOUND, request);
+
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+
+    @ExceptionHandler({InvalidUserException.class})
+    protected ResponseEntity<Object> handleInvalidUserException (InvalidUserException exception, WebRequest request) {
+        return handleExceptionInternal(exception,
+                request.getDescription(false) + " : " +  exception.getMessage(),
+                new HttpHeaders(),
+                HttpStatus.FORBIDDEN, request);
+
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+
+    @ExceptionHandler(NoSuchElementException.class)
+    protected ResponseEntity<Object> handleNoSuchElementException (NoSuchElementException exception, WebRequest request) {
+        return handleExceptionInternal(exception,
+                request.getDescription(false) + " : " +  exception.getMessage(),
+                new HttpHeaders(),
+                HttpStatus.NOT_FOUND, request);
+
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    protected ResponseEntity<Object> handleRuntimeException(RuntimeException exception, WebRequest request) {
+        return handleExceptionInternal(exception, exception.getMessage(),
+                new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+
+//        return ResponseEntity
+//                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                .body(exception.getMessage());
+    }
+
+
+}
