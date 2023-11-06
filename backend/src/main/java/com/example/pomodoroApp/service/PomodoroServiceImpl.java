f0dfeb9ba50da960e97298e51d8a63091e619cbd
Adrian Yates <118460049+adeyatz@users.noreply.github.com>
2023-11-06 18:39:53 +0000

    feat: PomodoroServiceImpl => impl. getTaskById amd updateTaskById methods

--- backend/src/main/java/com/example/pomodoroApp/service/PomodoroServiceImpl.java
+++ backend/src/main/java/com/example/pomodoroApp/service/PomodoroServiceImpl.java
@@ -1,17 +1,33 @@
 package com.example.pomodoroApp.service;
 
+import com.example.pomodoroApp.exceptions.InvalidTaskIdException;
+import com.example.pomodoroApp.exceptions.InvalidUserException;
 import com.example.pomodoroApp.model.UserPomodoroTask;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class PomodoroServiceImpl implements PomodoroService {
 
+    @Autowired
+    PomodoroRepository pomodoroRepository;
+
     @Override
     public List<UserPomodoroTask> getAllTasks(String uid, boolean isCompleted) {
-        return null;
+
+        // Validate uid
+        if (!isValidUid(uid)) {
+            throw new InvalidUserException("User ID is not valid");
+        }
+
+        List<UserPomodoroTask> tasks = new ArrayList<>();
+        pomodoroRepository.findAll().forEach(tasks::add);
+        return tasks;
     }
 
     @Override
@@ -31,11 +47,37 @@ public class PomodoroServiceImpl implements PomodoroService {
 
     @Override
     public UserPomodoroTask updateTaskById(String uid, Long taskId, UserPomodoroTask task) {
-        return null;
+
+        if (!isValidUid(uid)) {
+            throw new InvalidUserException("User ID is not valid");
+        }
+
+        UserPomodoroTask retrievedTask = getTaskById(taskId);
+
+        if (retrievedTask != null) {
+            retrievedTask.setTitle(task.getTitle());
+            retrievedTask.setDescription(task.getDescription());
+            retrievedTask.setGoogleEventId(task.getGoogleEventId());
+            retrievedTask.setGoogleUserId(task.getGoogleUserId());
+            retrievedTask.setCalendarStartDateTime(task.getCalendarStartDateTime());
+            retrievedTask.setPomodoroStartDateTime(task.getPomodoroStartDateTime());
+            retrievedTask.setPomodoroEndDateTime(task.getPomodoroEndDateTime());
+            pomodoroRepository.save(retrievedTask);
+        } else {
+            throw new InvalidTaskIdException ( "The task with id " + taskId + " cannot be found");
+        }
+        return retrievedTask;
     }
 
     @Override
     public UserPomodoroTask getTaskById(Long taskId) {
-        return null;
+        return pomodoroRepository.findById(taskId)
+                .orElseThrow(() -> new InvalidTaskIdException ("No task present with ID = " + taskId));
+    }
+
+
+    private boolean isValidUid (String uid) {
+        // TODO - call database to see if UID exists in db. True if it exists, false otherwise
+        return pomodoroRepository.isValidUid (uid);
     }
 }
