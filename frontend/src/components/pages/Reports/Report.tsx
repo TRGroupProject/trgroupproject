abcc1d321983c5f7c12bdb96bca575e7b1880d84
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-11-07 02:09:14 +0000

    Feat: Create all components as layout

---
+++ frontend/src/components/pages/Reports/Report.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+interface IReportProps {
+}
+
+const Report: React.FC<IReportProps> = () => {
+  return <>Report</>;
+};
+
+export default Report;
---
+++ frontend/src/components/pages/TasksList.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+interface ITaskListProps {
+}
+
+const TaskList: React.FC<ITaskListProps> = () => {
+  return <>TaskList</>;
+};
+
+export default TaskList;
