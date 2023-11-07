36748a487d93bd4f5f20782c52899e4c05b09540
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-11-07 01:15:56 +0000

    Refactor: Move code for Google api calls from controller folder to service folder

--- backend/src/main/java/com/example/pomodoroApp/service/PomodoroAppService.java
+++ backend/src/main/java/com/example/pomodoroApp/service/PomodoroAppService.java
@@ -2,8 +2,11 @@ package com.example.pomodoroApp.service;
 
 import com.example.pomodoroApp.model.UserPomodoroTask;
 
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 
 public interface PomodoroAppService {
     public List<UserPomodoroTask> getAllTasksByGoogleUserId(String googleUserId);
@@ -13,4 +16,8 @@ public interface PomodoroAppService {
     public List<UserPomodoroTask> getAllUncompletedTasksByGoogleUserId(String googleUserId);
 
     public URL getGoogleApiUrl(String uid);
+
+    public String getGoogleApiCalendarEvents(String uid) throws URISyntaxException, ExecutionException, InterruptedException, TimeoutException;
+
+    public String getGoogleApiUserInfo(String uid) throws URISyntaxException, ExecutionException, InterruptedException, TimeoutException;
 }
