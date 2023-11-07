6809bf6c5e9031471b035360aa1f062629f682e2
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-11-07 02:06:01 +0000

    feat: Add BrowserRouter, GoogleOAuthProvider, client id and Router to app.tsx. And update gitignore

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter } from "react-router-dom";
+import {Router} from "./components/router/Router";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const client_id = import.meta.env.VITE_REACT_APP_CLIENT_ID;
+
 const App: React.FC = () => {
 
   return (
     <>
+     <GoogleOAuthProvider clientId={client_id}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </GoogleOAuthProvider>
     </>
   )
 }
