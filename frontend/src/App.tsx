aaa9907f9b97c745d3c7b9989e8af1dcb797fb23
chadrakh <chadrak@outlook.com>
2023-11-08 15:54:15 +0000

    Feat: Add App Title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import {Router} from "./components/router/Router";
 import { GoogleOAuthProvider } from '@react-oauth/google';
@@ -5,6 +6,9 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 const client_id = import.meta.env.VITE_REACT_APP_CLIENT_ID;
 
 const App: React.FC = () => {
+  useEffect(() => {
+    document.title="Pomodoro App";
+  });
 
   return (
     <>
