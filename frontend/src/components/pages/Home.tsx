d267a616d939b22cb64bd845443f9480d20e67b8
chadrakh <chadrak@outlook.com>
2023-11-08 18:04:50 +0000

    feat: Implement API call logic in Home component

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -1,21 +1,69 @@
+import { useEffect, useState } from "react";
+import styled from "@emotion/styled";
 import { useGoogleLogin } from "@react-oauth/google";
 import Button from "../features/Buttons/Button";
+import googleIcon from "../../assets/google-icon.png";
+import LayoutCard from "../features/Layout/LayoutCard";
+import { useNavigate } from "react-router-dom";
+
+const Title = styled.h2`
+  font-size: 24px;
+  color: #333;
+`;
 
 const Home: React.FC = () => {
+  const navigate = useNavigate()
+  const [credentials, setCredentials] = useState('');
+
   const login = useGoogleLogin({
-    onSuccess: (codeResponse) => console.log(codeResponse),
-    scope: "https://www.googleapis.com/auth/calendar.readonly",
-    onError: (error) => console.error("Login Authentication Failed", error)
+    onSuccess: (codeResponse) => {
+      console.log(codeResponse);
+      setCredentials(codeResponse.token_type + codeResponse.access_token);
+      navigate("/tasks");
+    },
+    scope: 'https://www.googleapis.com/auth/calendar.readonly',
+    onError: (error) => console.error('Login Authentication Failed', error),
   });
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = 'http://localhost:8080/api/v1/events/users';
+      const headers = new Headers();
+      headers.append('Authorization', credentials);
+      headers.append('Connection', 'keep-alive');
+      headers.append('Access-Control-Allow-Origin', 'true');
+
+      try {
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: headers,
+        });
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        console.log('data', data);
+
+        return data;
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (credentials) {
+      fetchData();
+    }
+  }, [credentials]);
+
   return (
-    <>
-      <Button 
-        handleOnClick={() => login()}
-      >
-        Login in with Google
+    <LayoutCard>
+      <Title>Login with Google</Title>
+      <Button icon={googleIcon} handleOnClick={() => login()}>
+        Authenticate
       </Button>
-    </>
+    </LayoutCard>
   );
 };
 
