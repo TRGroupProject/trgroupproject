b16e3190093ab9dc7d929a03ad1a9a8d13805bca
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-11-08 15:54:13 +0000

    Feat: Add context provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,43 @@
 import { BrowserRouter } from "react-router-dom";
 import {Router} from "./components/router/Router";
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { TasksContext } from "./hooks/useContext/taskcontext";
+import { useEffect, useState } from "react";
+import useFetchTasks from "./hooks/useFetchTasks";
 
 const client_id = import.meta.env.VITE_REACT_APP_CLIENT_ID;
 
 const App: React.FC = () => {
 
+  const endpoint = 'http://localhost:8080/api/v1/tests/';
+  const { data } = useFetchTasks(endpoint);
+
+  type UserPomodoroTask = {
+    taskId: string;
+    googleUserId: string;
+    googleEventId: string;
+    title: string;
+    description: string;
+    calendarStartDateTime: string;
+    pomodoroStartDateTime: string | null;
+    pomodoroEndDateTime: string | null;
+  }
+
+  useEffect(() => {
+    setTasks(data);
+    }, [data]);
+
+  const [ tasks, setTasks] = useState<UserPomodoroTask[]>(data)
+
   return (
     <>
-     <GoogleOAuthProvider clientId={client_id}>
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
-      </GoogleOAuthProvider>
+      <TasksContext.Provider value={tasks}>
+        <GoogleOAuthProvider clientId={client_id}>
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+        </GoogleOAuthProvider>
+      </TasksContext.Provider>
     </>
   )
 }
